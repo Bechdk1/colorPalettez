4e68a952f9602345962ad07e736eb255c7b2c722
Yliubov <129996599+YLiubov@users.noreply.github.com>
2026-03-10 09:55:48 +0100

    commiot

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import "../tailwind/style.css";
 import { header } from "./header.ts";
-
+import { mainLayout } from "./mainLayout.ts";
+import { colorCard } from "./colorCard.ts";
 
 const body = document.getElementById("body")!;
 
-body.append(header);
+body.append(header, mainLayout);
 
+const palette = document.querySelector("#palette");
+
+// Массив цветов
+// Это одна тестовая палитра
+const colors = [
+  "#CF9407",
+  "#F6EDB7",
+  "#BDBF63",
+  "#601E3E",
+  "#7C7582"
+];
+
+// Создай карточку цвета
+// Возьми каждый цвет из массива по очереди”
+colors.forEach((color) => {
+// Добавь готовую карточку в секцию палитры”
+  palette?.append(colorCard(color));
+});
---
+++ src/mainLayout.ts
@@ -0,0 +1,7 @@
+export let mainLayout = document.createElement ("main")
+
+// create section id="palette"
+const palette = document.createElement("section")
+palette.id = "palette"
+
+mainLayout.append(palette)
