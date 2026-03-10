fd90aff49e1fe26de989754320ce404288233018
Yliubov <129996599+YLiubov@users.noreply.github.com>
2026-03-10 13:50:49 +0100

    colorCard

--- src/components/colorCard.ts
+++ src/components/colorCard.ts
@@ -1,9 +1,43 @@
-export function colorCard() {
+import { fetchData } from "../data/api.ts"
 
-  const generateBtn = document.createElement ("button")
-  
-  const function generateCard(colorArray) {
-  let cardContent = document.createElement ("figure")
+export function colorCard() {
+  const generateBtn = document.createElement("button")
+  generateBtn.textContent = "Generate Paletes"
+  generateBtn.className = buttonClass
+
+  let currentCard: HTMLElement | null = null | // Defining the card as a html-elemrnt or as nothing
+
+  generateBtn.addEventListener("click", async () => {
+    const data = await fetchData()
+    console.log("fetchData:", data)
+    if (!data) return
+
+    const card = generateCard(data.result)
+    if (currentCard) currentCard.replaceWith(card)
+    else document.body.appendChild(card)
+    currentCard = card
+  })
+
+  function generateCard(colorArray: number[][]): HTMLElement {
+    const card = document.createElement ("figure")
+
+    colorArray.forEach ((rgb) => {
+      const wrapper = document.createElement("div")
+
+      const colorSub = document.createElement("div")
+      colorSub.classList = "bg-[rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]}]"
+
+      const hexSub = 
+        "#" + rgb.map((v) => v.toString(16).padStart(2,"0")).join("")
+        const hexText = document.createElement("h3")
+        hexText.innerText = hexSub
+
+        wrapper.append(colorSub, hexText)
+        card.appendChild(wrapper)
+    })
+
+    return card
   }
 
+  return generateBtn
 }
